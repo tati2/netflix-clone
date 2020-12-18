da2fb0c00ba9356ec5ec3cd868b809ac5e7bfb73
Tatiana Correia <taty.correia.s29@gmail.com>
2020-12-18 12:13:31 -0300

    ajuste-layout-responsivo

--- src/App.js
+++ src/App.js
@@ -6,49 +6,49 @@ import './App.css'
 
 class Filme extends Component {
 
- state = {
-   movies: [],
-   series:[]
+  state = {
+    movies: [],
+    series: []
 
- }
+  }
 
-async componentDidMount(){
-  const response = await axios.get(`${process.env.REACT_APP_API}/movies`);
-  
-  this.setState({
-    movies: response.data
-  });
+  async componentDidMount() {
+    const response = await axios.get(`${process.env.REACT_APP_API}/movies`);
 
-  const filmes= response.data.map((item) => {
-    return {
-    ...item,
-    poster_path: `https://image.tmdb.org/t/p/w500${item.poster_path}`
+    this.setState({
+      movies: response.data
+    });
 
-    };    
-  });
-  
-  this.setState({
-    movies:filmes
-  });
+    const filmes = response.data.map((item) => {
+      return {
+        ...item,
+        poster_path: `https://image.tmdb.org/t/p/w500${item.poster_path}`
 
-  const series = await axios.get(`${process.env.REACT_APP_API}/shows`);
-  
-  this.setState({
-    series: series.data    
-  });
+      };
+    });
 
-  const shows = series.data.map((item) => {
-    return {
-    ...item,
-    poster_path: `https://image.tmdb.org/t/p/w500${item.poster_path}`
+    this.setState({
+      movies: filmes
+    });
 
-    };    
-  });
+    const series = await axios.get(`${process.env.REACT_APP_API}/shows`);
 
-  this.setState({
-    series:shows
-  })
-}
+    this.setState({
+      series: series.data
+    });
+
+    const shows = series.data.map((item) => {
+      return {
+        ...item,
+        poster_path: `https://image.tmdb.org/t/p/w500${item.poster_path}`
+
+      };
+    });
+
+    this.setState({
+      series: shows
+    })
+  }
 
   render() {
     return (
@@ -56,30 +56,31 @@ async componentDidMount(){
         <div className="box-title">
           <h1>TTN CINE</h1>
         </div>
+        <p className="classificacao-filmes">Filmes</p>
         <div className="box-map-filmes">
-          <p className="classificacao-filmes">Filmes</p>
           {this.state.movies.map((item, index) => (
-            <div className="box-return-map"  key={index} >
-              <h2 className="names-filmes">{item.title}</h2>
-              <img className="img-filmes" src={item.poster_path} alt=""/>
-              <p className="descricao">{item.overview}</p>
-              <p className="data">{item.release_date}</p>
+            <div className="box-return-map" key={index} >
+              <img className="img-filmes" src={item.poster_path} alt="" />
+              <div>
+                <p>{item.vote_average}</p>
+                <p>{item.release_date}</p>
+              </div>
             </div>
           ))}
-        </div> 
-        <div className="box-map-series">
+        </div>
           <p className="classificacao-serie">Séries</p>
+        <div className="box-map-series">
           {this.state.series.map((item, index) => (
             <div className="box-return-map-series" key={index}>
-              <h2 className="names-serie">{item.name}</h2>
-              <img className="img-series" src={item.poster_path} alt=""/>
-              <p className="descricao-serie">{item.overview}</p>
-              <p className="data-serie">{item.first_air_date}</p>
+              <img className="img-series" src={item.poster_path} alt="" />
+              <div>
+                <p>{item.vote_average}</p>
+                <p>{item.first_air_date}</p>
+              </div>              
             </div>
           ))}
-        </div>    
-      </div> 
-     
+        </div>
+      </div>
     );
   }
 }
